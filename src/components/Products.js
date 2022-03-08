a3298eb9d238c8228e81ab93f1043237ae6ce5cd
thienchanvota1312 <37357766+thienchanvota1312@users.noreply.github.com>
2022-03-08 12:39:44 +0700

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faHeart} from "@fortawesome/free-solid-svg-icons"
-
+import React, { Component } from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Tables extends Component {
   constructor(props) {
     super(props)
     this.state = {
       products: [
-        { Name: 'Car 1', Brand: 'Audi', Stock: 7, rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
-        { Name: 'Car 2', Brand: 'Suzuki', Stock: 5, rate: 2.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
-        { Name: 'Car 3', Brand: 'Vinfast', Stock: 8, rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
-        { Name: 'Car 4', Brand: 'Mercedes', Stock: 7, rate: 4.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
+        { id: 1, Name: 'Car 1', Brand: 'Audi', Stock: 7, Rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button type="button" class="btn btn-danger">Delete</button> },
+        { id: 2, Name: 'Car 2', Brand: 'Suzuki', Stock: 5, Rate: 2.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button type="button" class="btn btn-danger">Delete</button> },
+        { id: 3, Name: 'Car 3', Brand: 'Vinfast', Stock: 8, Rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button type="button" class="btn btn-danger">Delete</button> },
+        { id: 4, Name: 'Car 4', Brand: 'Mercedes', Stock: 7, Rate: 4.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button type="button" class="btn btn-danger">Delete</button> },
       ],
     }
   }
-  handleDelete = (products) => {
-  const newsName = products.Name;
-  this.setState(prevState => ({
-    news: prevState.news.filter(elm => elm.Name !== newsName )
-  }));
-}
 
   renderTableData() {
     return this.state.products.map((product, index) => {
-      const { Name, Brand, Stock, Rate, Like, Action } = product
+      const { id ,Name, Brand, Stock, Rate, Like, Action } = product
       return (
-        <tr key={Name}>
+        <tr key={id}>
+          <td>{id}</td>
           <td>{Name}</td>
           <td>{Brand}</td>
           <td>{Stock}</td>
@@ -44,15 +39,8 @@ class Tables extends Component {
   }
 
   render() {
-    const myStyle = {
-      color: "black",
-      backgroundColor: "white",
-      padding: "10px",
-      fontFamily: "Sans-Serif",
-      border: "1px solid black",
-    };
     return (
-        <table style={myStyle}>
+        <table className ="table">
           <tbody>
             <tr>{this.renderTableHeader()}</tr>
             {this.renderTableData()}
