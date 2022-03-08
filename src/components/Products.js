a95acdbf3576ea2ab597ce07762659d15c49d456
thienchanvota1312 <37357766+thienchanvota1312@users.noreply.github.com>
2022-03-08 09:35:41 +0700

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,14 +8,19 @@ class Tables extends Component {
     super(props)
     this.state = {
       products: [
-        { Name: 'Car 1', Brand: 'Audi', Stock: 7, rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}}>Delete</button> },
-        { Name: 'Car 2', Brand: 'Suzuki', Stock: 5, rate: 2.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}}>Delete</button> },
-        { Name: 'Car 3', Brand: 'Vinfast', Stock: 8, rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}}>Delete</button> },
-        { Name: 'Car 4', Brand: 'Mercedes', Stock: 7, rate: 4.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}}>Delete</button> },
+        { Name: 'Car 1', Brand: 'Audi', Stock: 7, rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
+        { Name: 'Car 2', Brand: 'Suzuki', Stock: 5, rate: 2.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
+        { Name: 'Car 3', Brand: 'Vinfast', Stock: 8, rate: 3.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
+        { Name: 'Car 4', Brand: 'Mercedes', Stock: 7, rate: 4.5, Like: <FontAwesomeIcon icon= {faHeart} /> , Action: <button style={{color:"white", backgroundColor: "red"}} onClick={() => this.handleDelete(products)>Delete</button> },
       ],
     }
   }
-
+  handleDelete = (products) => {
+  const newsName = products.Name;
+  this.setState(prevState => ({
+    news: prevState.news.filter(elm => elm.Name !== newsName )
+  }));
+}
 
   renderTableData() {
     return this.state.products.map((product, index) => {
